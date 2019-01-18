397af5aff2848718e98cda75e7c48d3bc8686247
Steve Smith <stvsmth@gmail.com>
2019-01-18 14:06:58 -0700

    Convert to pytest

--- tests.py
+++ tests.py
@@ -1,6 +1,3 @@
-
-import unittest
-
 from kenpom import parse_data
 
 
@@ -10,53 +7,47 @@ def mock_fetch_content():
         return data
 
 
-class KenpomTestCase(unittest.TestCase):
+def test_parse_data():
+    page_content = mock_fetch_content()
+    all_data = parse_data(page_content)
 
-    def test_parse_data(self):
-        page_content = mock_fetch_content()
-        all_data = parse_data(page_content)
+    # Test the first row, all columns to make sure we're matched
+    assert 'Virginia' == all_data[0].name
+    assert '1' == all_data[0].rank
+    assert 'ACC' == all_data[0].conf
+    assert '15-0' == all_data[0].record
+    assert '+32.80' == all_data[0].eff_margin
+    assert '117.9' == all_data[0].offense
+    assert '6' == all_data[0].off_rank
+    assert '85.1' == all_data[0].defense
+    assert '2' == all_data[0].def_rank
 
-        # Test the first row, all columns to make sure we're matched
-        self.assertEqual('Virginia', all_data[0].name)
-        self.assertEqual('1', all_data[0].rank)
-        self.assertEqual('ACC', all_data[0].conf)
-        self.assertEqual('15-0', all_data[0].record)
-        self.assertEqual('+32.80', all_data[0].eff_margin)
-        self.assertEqual('117.9', all_data[0].offense)
-        self.assertEqual('6', all_data[0].off_rank)
-        self.assertEqual('85.1', all_data[0].defense)
-        self.assertEqual('2', all_data[0].def_rank)
+    assert '61.0' == all_data[0].tempo
+    assert '353' == all_data[0].tempo_rank
+    assert '+.054' == all_data[0].luck
+    assert '80' == all_data[0].luck_rank
 
-        self.assertEqual('61.0', all_data[0].tempo)
-        self.assertEqual('353', all_data[0].tempo_rank)
-        self.assertEqual('+.054', all_data[0].luck)
-        self.assertEqual('80', all_data[0].luck_rank)
+    assert '+1.22' == all_data[0].sos_eff_margin
+    assert '128' == all_data[0].sos_eff_margin_rank
+    assert '102.4' == all_data[0].sos_off
+    assert '227' == all_data[0].sos_off_rank
+    assert '101.2' == all_data[0].sos_def
+    assert '62' == all_data[0].sos_def_rank
 
-        self.assertEqual('+1.22', all_data[0].sos_eff_margin)
-        self.assertEqual('128', all_data[0].sos_eff_margin_rank)
-        self.assertEqual('102.4', all_data[0].sos_off)
-        self.assertEqual('227', all_data[0].sos_off_rank)
-        self.assertEqual('101.2', all_data[0].sos_def)
-        self.assertEqual('62', all_data[0].sos_def_rank)
+    assert '-2.92' == all_data[0].sos_non_conf
+    assert '261' == all_data[0].sos_non_conf_rank
 
-        self.assertEqual('-2.92', all_data[0].sos_non_conf)
-        self.assertEqual('261', all_data[0].sos_non_conf_rank)
+    # ... sentimental check
+    assert 'Virginia Tech' == all_data[6].name
+    assert '7' == all_data[6].rank
+    assert '323' == all_data[6].sos_non_conf_rank
 
-        # ... sentimental check
-        self.assertEqual('Virginia Tech', all_data[6].name)
-        self.assertEqual('7', all_data[6].rank)
-        self.assertEqual('323', all_data[6].sos_non_conf_rank)
+    # Quick test row before the header
+    assert 'Wofford' == all_data[39].name
+    assert '40' == all_data[39].rank
+    assert '17' == all_data[39].sos_non_conf_rank
 
-        # Quick test row before the header
-        self.assertEqual('Wofford', all_data[39].name)
-        self.assertEqual('40', all_data[39].rank)
-        self.assertEqual('17', all_data[39].sos_non_conf_rank)
-
-        # Quick test row after the header
-        self.assertEqual('Butler', all_data[40].name)
-        self.assertEqual('41', all_data[40].rank)
-        self.assertEqual('65', all_data[40].sos_non_conf_rank)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    # Quick test row after the header
+    assert 'Butler' == all_data[40].name
+    assert '41' == all_data[40].rank
+    assert '65' == all_data[40].sos_non_conf_rank
